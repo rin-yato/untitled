e7450ed21b92b23ec609bf0b354e5f8cfc52bea3
rin-yato <chearithorn@gmail.com>
2023-05-23 12:53:42 +0700

    feat: added table service page and table card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
-import ToastDemo from "@/components/toast-demo"
+"use client"
+
+import { useState } from "react"
+import { useAutoAnimate } from "@formkit/auto-animate/react"
+
+import Text from "@/components/ui/text"
+import PickMenuDialog from "@/components/dialog/pick-menu-dialog"
+import { PickTableDialog } from "@/components/dialog/pick-table-dialog"
+import TableCard from "@/components/table-card"
 
 export default function IndexPage() {
+  const [tables, setTables] = useState<number[]>([])
+  const [parent] = useAutoAnimate({
+    duration: 250,
+    easing: "ease",
+  })
+
   return (
-    <section className="flex min-h-screen items-center justify-center">
-      <ToastDemo />
+    <section className="flex flex-1 flex-col items-start justify-start overflow-y-auto px-5 pb-10 pt-8">
+      <Text
+        variant="heading"
+        className="mb-11 select-none underline underline-offset-4"
+      >
+        Table
+      </Text>
+      <div ref={parent} className="grid w-full grid-cols-4 gap-5">
+        {tables.map((table, index) => (
+          <TableCard key={index} id={table} />
+        ))}
+        <PickTableDialog
+          filter={tables}
+          onPickTable={(id) => setTables([...tables, id])}
+        />
+      </div>
     </section>
   )
 }
