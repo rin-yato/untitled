d13a92999ff1016f941f0148c8d82050f4207d98
rin-yato <chearithorn@gmail.com>
2023-05-23 21:18:51 +0700

    feat: added receipt atom and useReceipt

---
+++ src/hooks/use-receipt.ts
@@ -0,0 +1,33 @@
+import receiptsAtom from "@/store/receipt-atom"
+import { useAtom } from "jotai"
+
+import { Receipt } from "@/types/entities/receipt"
+
+export default function useReceipt() {
+  const [receipts, setReceipts] = useAtom(receiptsAtom)
+
+  const addReceipt = (tableId: number) => {
+    const mockReceipt: Receipt = {
+      id: Math.random(),
+      tableId,
+      icons: [],
+      orderedItems: [],
+    }
+    setReceipts([...receipts, mockReceipt])
+  }
+
+  const updateItem = (receiptId: number, itemId: number, quantity: number) => {
+    const receipt = receipts.find((receipt) => receipt.id === receiptId)
+    if (!receipt) return
+    const item = receipt.orderedItems.find((item) => item.itemId === itemId)
+    if (!item) return
+    item.quantity = quantity
+    setReceipts([...receipts])
+  }
+
+  return {
+    receipts,
+    addReceipt,
+    updateItem,
+  }
+}
