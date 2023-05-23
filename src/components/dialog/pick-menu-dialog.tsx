90c85112a4d773b153d939a72bb663d99a1db6fd
rin-yato <chearithorn@gmail.com>
2023-05-23 12:54:26 +0700

    feat: added dialog for picking from menus

---
+++ src/components/dialog/pick-menu-dialog.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import React from "react"
+import { categories } from "@/db"
+import { Close } from "@radix-ui/react-dialog"
+
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import Text from "@/components/ui/text"
+import { Icons } from "@/components/icons"
+import MenuTab from "@/components/menu-tab"
+
+export default function PickMenuDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="gap-2" size="xl">
+          <Icons.plus size="20" />
+          Add Item
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="dialog-no-close custom-scrollbar h-[70vh] w-[65vw] !max-w-none overflow-y-auto !rounded-2xl py-6 ">
+        <MenuTab categories={categories} />
+      </DialogContent>
+    </Dialog>
+  )
+}
