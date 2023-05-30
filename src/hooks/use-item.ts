77dc760b8fb3c1c9300f73eaba0c15fca9348d2a
rin-yato <chearithorn@gmail.com>
2023-05-30 15:35:28 +0700

    fixed come stuffs

--- src/hooks/use-item.ts
+++ src/hooks/use-item.ts
@@ -1,28 +1,28 @@
 
 
-// import { useEffect } from "react"
-// import { itemsAtom } from "@/jotai/items-atom"
-// import { useAtom } from "jotai"
+import { useEffect } from "react"
+import { itemsAtom } from "@/jotai/items-atom"
+import { useAtom } from "jotai"
 
-// export default function useItem() {
-//   const [items, setItems] = useAtom(itemsAtom)
+export default function useItem() {
+  const [items, setItems] = useAtom(itemsAtom)
 
-//   function fetchItems() {
-//     fetch("/api/item").then((res) => {
-//       if (res.ok) {
-//         res.json().then((data) => {
-//           setItems(data)
-//         })
-//       }
-//     })
-//   }
+  function fetchItems() {
+    fetch("/api/item").then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setItems(data)
+        })
+      }
+    })
+  }
 
-//   useEffect(() => {
-//     fetchItems()
-//   }, [])
+  useEffect(() => {
+    fetchItems()
+  }, [])
 
-//   return {
-//     items,
-//     fetchItems,
-//   }
-// }
+  return {
+    items,
+    fetchItems,
+  }
+}
